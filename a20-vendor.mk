ef8f5511b7de51a6880995e6826c3fb54ae5d83a
royna2544 <whiteshell2544@naver.com>
2025-04-08 13:20:13 +0900

    a20: Remove universal7884B from modules
    - Add 32bit sensor hw module

--- a20-vendor.mk
+++ a20-vendor.mk
@@ -40,7 +40,8 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/a20/proprietary/vendor/lib/hw/gralloc.exynos7884B.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/gralloc.exynos7884B.so \
     vendor/samsung/a20/proprietary/vendor/lib/hw/hwcomposer.exynos7884B.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/hwcomposer.exynos7884B.so \
     vendor/samsung/a20/proprietary/vendor/lib/hw/memtrack.exynos7884B.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/memtrack.exynos7884B.so \
-    vendor/samsung/a20/proprietary/vendor/lib/hw/thermal.universal7884B.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/thermal.universal7884B.so \
+    vendor/samsung/a20/proprietary/vendor/lib/hw/sensors.exynos7884B.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/sensors.exynos7884B.so \
+    vendor/samsung/a20/proprietary/vendor/lib/hw/thermal.exynos7884B.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/thermal.exynos7884B.so \
     vendor/samsung/a20/proprietary/vendor/lib/libOpenCv.camera.samsung.so:$(TARGET_COPY_OUT_VENDOR)/lib/libOpenCv.camera.samsung.so \
     vendor/samsung/a20/proprietary/vendor/lib/libSEF.quram.so:$(TARGET_COPY_OUT_VENDOR)/lib/libSEF.quram.so \
     vendor/samsung/a20/proprietary/vendor/lib/lib_SamsungRec_07001.so:$(TARGET_COPY_OUT_VENDOR)/lib/lib_SamsungRec_07001.so \
@@ -57,8 +58,8 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/a20/proprietary/vendor/lib64/hw/gralloc.exynos7884B.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/gralloc.exynos7884B.so \
     vendor/samsung/a20/proprietary/vendor/lib64/hw/hwcomposer.exynos7884B.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/hwcomposer.exynos7884B.so \
     vendor/samsung/a20/proprietary/vendor/lib64/hw/memtrack.exynos7884B.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/memtrack.exynos7884B.so \
-    vendor/samsung/a20/proprietary/vendor/lib64/hw/sensors.universal7884B.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/sensors.universal7884B.so \
-    vendor/samsung/a20/proprietary/vendor/lib64/hw/thermal.universal7884B.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/thermal.universal7884B.so \
+    vendor/samsung/a20/proprietary/vendor/lib64/hw/sensors.exynos7884B.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/sensors.exynos7884B.so \
+    vendor/samsung/a20/proprietary/vendor/lib64/hw/thermal.exynos7884B.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/thermal.exynos7884B.so \
     vendor/samsung/a20/proprietary/vendor/lib64/hw/vendor.samsung.hardware.gnss@2.0-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/vendor.samsung.hardware.gnss@2.0-impl.so \
     vendor/samsung/a20/proprietary/vendor/lib64/libOpenCv.camera.samsung.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libOpenCv.camera.samsung.so \
     vendor/samsung/a20/proprietary/vendor/lib64/libSEF.quram.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libSEF.quram.so \
